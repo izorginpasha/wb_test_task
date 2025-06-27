d8927751d4275e076f90023e64e16f1a3c1399dc
izorgin.pasha <izorgin.pasha@yandex.ru>
2025-06-27 14:15:35 +0500

    add api

--- backend/api.py
+++ backend/api.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 from schemas import ProductResponse
-
+from backend.db.db import db_dependency
 api_router = APIRouter(prefix="/products", tags=['products'])
 
-api_router.get("/", response_model=list[ProductResponse])
-async def get_habits_endpoint(user_id: int, db: db_dependency, current_user: str = Depends(get_current_user)):
-    return await get_habits(db, user_id)
+@api_router.get("/", response_model=list[ProductResponse])
+async def get_products(db: db_dependency,products:ProductResponse ):
+    return await get_products(db, products)
